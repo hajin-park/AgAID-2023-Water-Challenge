c28ca789ee4e9b3d7a326fa735d1cb9744a5f4fa
Hajin Park <hajin1819@gmail.com>
2023-01-28 21:10:55 -0800

    Added code comments for clarity

--- data_parser.py
+++ data_parser.py
@@ -3,22 +3,33 @@ import json
 
 
 filtered_data = []
+
+#   Training dataset template
 dataset = {
     "input": [],
     "target": 0
 }
+
+#   Record snapshots of 13 months (8 input months + 5 target months) while scanning the csv file
 curr_months = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
 months = ["January", "February", "March", "April", "May",
           "June", "July", "August", "September", "October", "November", "December"]
 
 
 def organize_data(line, curr_line, dataset_copy, header):
+    '''Record new month into the snapshot. Organize input and target month data for every data set.'''
+
+    #   Climate Index values used as input data for each training set
     input_types = {header[i]: var for i, var in enumerate(line[1:], 1)}
+
+    #   Translate dates into the name of the month as an input variable
     input_types['date'] = months[int(line[0].split("-")[1])-1]
 
+    #   Cycle in the newest month and remove the oldest one in each snapshot
     curr_months.pop(0)
     curr_months.insert(len(curr_months), input_types)
 
+    #   Start adding training data sets once line 13 is reached (for a complete data set)
     if curr_line > 12:
         dataset_copy["input"] = curr_months[:8]
         dataset_copy["target"] = sum(
